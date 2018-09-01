d2dc574c9fbcb6264dd4a96bcbb5ac03f8b5c8a9
Tarik Mey <tarik-mey@gmx.de>
2018-09-01 17:49:14 -0400

    commented out mongoose

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+// const mongoose = require('mongoose');
+// mongoose.Promise = global.Promise;
 
 app.use(express.static('public'));
 

cb561a89fba10f3d5004b5d22640a622b7c213cc
Tarik Mey <tarik-mey@gmx.de>
2018-09-02 07:30:20 -0400

    integrated cors and defined the endpoint for the task lists or boards

--- server.js
+++ server.js
@@ -2,17 +2,58 @@
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const {CLIENT_ORIGIN} = require('./config');
 // const mongoose = require('mongoose');
 // mongoose.Promise = global.Promise;
 
-app.use(express.static('public'));
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
 
-const PORT = process.env.PORT || 3000;
+const board = {
+    lists: [
+        {
+            title: 'Example list 1',
+            cards: [
+                {
+                    text: 'Example card 1'
+                },
+                {
+                    text: 'Example card 2'
+                }
+            ]
+        },
+        {
+            title: 'Example list 2',
+            cards: [
+                {
+                    text: 'Example card 1'
+                },
+                {
+                    text: 'Example card 2'
+                }
+            ]
+        }
+    ]
+};
 
-app.get('/api/*', (req, res) => {
-    res.json({ok: true});
-});
+app.get('/api/board', (req, res) => {
+    res.json(board);
+})
 
- app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(8080);
 
- module.exports = {app};
+// app.use(express.static('public'));
+
+// const PORT = process.env.PORT || 3000;
+
+// app.get('/api/*', (req, res) => {
+//     res.json({ok: true});
+// });
+
+//  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+//  module.exports = {app};
